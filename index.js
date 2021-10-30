49e2ce57af18fa339875326c8f87fb5eca99a8bd
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-30 17:12:56 +0600

    add delete my order

--- index.js
+++ index.js
@@ -24,17 +24,50 @@ async function run() {
         // database and collection
         const database = client.db('travelo_db');
         const destinationCollection = database.collection('destinationrs');
+        const ordersCollection = database.collection('orders');
 
         // insert destination
         app.post('/addDestination', async (req, res) => {
             const destination = req.body;
             const result = await destinationCollection.insertOne(destination);
-            console.log(result);
             res.json(result);
         })
 
         // read destination
-        app.get()
+        app.get('/destination', async (req, res) => {
+            const result = await destinationCollection.find({}).toArray();
+            res.send(result)
+        })
+
+        // insert destination
+        app.post('/addOrders', async (req, res) => {
+            const orders = req.body;
+            const result = await ordersCollection.insertOne(orders);
+            console.log(result);
+            res.json(result);
+        })
+
+        // read manageOrders
+        app.get('/manageOrders', async (req, res) => {
+            const result = await ordersCollection.find({}).toArray();
+            res.send(result)
+        })
+
+        // read myOrders
+        app.get('/MyOrders/:email', async (req, res) => {
+            const email = req.params.email;
+            const result = await ordersCollection.find({ email: email }).toArray();
+            res.send(result)
+        })
+
+        // delete myOrder
+        app.delete('/myOrder/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
 
     }
     finally {
